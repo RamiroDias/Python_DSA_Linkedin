0a9978b4ab58611fee1d0e33fd13428464de56d9
RamiroDias <ramiro_v_dias@hotmail.com>
2025-09-07 17:52:09 +0100

    Did Stack section

--- Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/reverse_string.py
+++ Ex_Files_Python_Data_Structures/Exercise Files/02_02_begin/reverse_string.py
@@ -11,5 +11,10 @@ reversed_string = ""
 s = stack.Stack()
 
 # Your solution here.
+for letter in string:
+    s.push(letter)
+
+while not s.is_empty():
+    reversed_string += s.pop()
 
 print(reversed_string)
